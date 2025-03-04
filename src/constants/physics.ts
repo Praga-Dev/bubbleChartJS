3f00bfdd633410a64a44c41410735b1d5d21b972
Pragadeeshwaran Pasupathi <pragadeeshwaran.pasupathi@gmail.com>
2025-03-04 09:01:25 +0530

    bubbles colliding related changes

--- src/constants/physics.ts
+++ src/constants/physics.ts
@@ -1,10 +1,10 @@
 export const PHYSICS = {
-  forceStrength: 0.015,
+  forceStrength: 0.008,
   iterations: 1000,
-  damping: 0.55,
+  damping: 0.65,
   boundaryForce: 0.02,
   centerForce: 0.12,
-  centerAttraction: 0.6,
+  centerAttraction: 0.8,
   centerDamping: 0.3,
   centerRadiusBuffer: 35,
 } as const;
